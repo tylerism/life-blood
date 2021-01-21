0f9bfd05f54e1d984271b652eb5bb6444ad4d29c
Tyler Pugh <tyler.pugh@rigup.com>
2021-01-20 20:58:32 -0600

    updates

---
+++ classes.js
@@ -0,0 +1,86 @@
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+class Warrior {
+    constructor (name, gold, weight, xp) {
+        this.name = name;
+        this.weight = weight;
+        this.gold = gold;
+        this.xp = xp;
+    }
+}
+
+class Reward {
+    constructor (text, gold) {
+        this.text = text;
+        this.gold = gold;
+    }
+}
+
+class Punishment {
+    constructor (text, gold) {
+        this.text = text;
+        this.gold = gold;
+    }
+}
+
+class Quest {
+    constructor (text, duration, rewards, punishments = [], id) {
+        this.text = text;
+        this.duration = duration;
+        this.rewards = rewards;
+        this.punishments = punishments;
+        this.id = id;
+    }
+    toHTML() {
+        let rewardHtml = "";
+        this.rewards.forEach(r => {
+            rewardHtml += r.text || r.gold;
+        });
+        return `<div class="quest${this.active ? ' active' : ''}" quest="${this.id}"><h2>${this.text}</h2><div class="duration"><span>Duration: </span> ${this.duration}</div><div class="reward"><span>Reward: </span>${rewardHtml}</div><button class="begin_quest">Begin</button><button class="cancel_quest">Cancel</button></div></div>`;
+    }
+}
+
+class WarriorQuest {
+    constructor(warrior, quest, active = false, completed = null, startDate = new Date(), id) {
+        this.warrior = warrior;
+        this.quest = quest;
+        this.active = active;
+        this.completed = completed;
+        this.startDate = startDate;
+        this.id = id
+    }
+    toHTML() {
+        let rewardHtml = "";
+        this.quest.rewards.forEach(r => {
+            rewardHtml += r.text || r.gold;
+        });
+        return `<div class="quest${this.active ? ' active' : ''}${this.completed === false ? ' failed' : ''}" quest="${this.id}"><h2>${this.quest.text}</h2><div class="duration"><span>Duration: </span> ${this.quest.duration}</div><div class="reward"><span>Reward: </span>${rewardHtml}</div><span>${this.startDate.toString()}</span><button class="begin_quest">Begin</button><button class="cancel_quest">Cancel</button></div></div>`;
+    }
+}
+
+class Attribute {
+    constructor (text, id) {
+        this.text = text;
+        this.id = id;
+    }
+    toHTML() {
+        return `<div class="attribute"><div class="attribute_text">${this.text}</div><div class="attribute_score">${this.score}</div></div>`;
+    }
+}
+
+class WarriorAttribute {
+    constructor (warrior, attribute, score) {
+        this.warrior = warrior;
+        this.attribute = attribute;
+        this.score = score;
+    }
+    toHTML() {
+        return `<div class="attribute"><div class="attribute_text">${this.text}</div><div class="attribute_score">${this.score}</div></div>`;
+    }
+}
+
---
+++ dbUtils.js
@@ -0,0 +1,94 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBCO4P_-ukuMhUzcExt4dRfSDSQ2Iwl524",
+    authDomain: "lifeblood-302217.firebaseapp.com",
+    projectId: "lifeblood-302217",
+    storageBucket: "lifeblood-302217.appspot.com",
+    messagingSenderId: "695724451254",
+    appId: "1:695724451254:web:d09d4c103358716f2e8d17"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+let warrior;
+let quests = [];
+let attributes = [];
+let rewards = [];
+let warriorQuests = [];
+
+// var questConverter = {
+// 	toFirestore: function(quest) {
+// 		return {
+// 			id: quest.name,
+// 			text: quest.state,
+// 			duration: quest.country,
+// 			rewards: quest.rewards
+// 			active: quest.active
+// 		};
+// 	},
+// 	fromFirestore: function(snapshot, options){
+// 		const data = snapshot.data(options);
+// 		return new Quest(data.id, data.text, data.duration, data.rewards, data.active);
+// 	}
+// };
+
+function populateData() {
+
+    db.collection("warriors").get().then((querySnapshot) => {
+        querySnapshot.forEach((w) => {
+            warrior = w.data();
+        });
+    });
+
+    // db.collection("warrior_quests").get().then((querySnapshot) => {
+    //     querySnapshot.forEach((quest) => {
+    //         const q = quest.data();
+    //         warriorQuests.push(new WarriorQuest(q.warrior, q.quest, q.active, q.completed, q.startDate))
+    //     });
+    //     buildWarriorQuests();
+    // });
+
+    // db.collection("attributes").get().then((querySnapshot) => {
+    //     querySnapshot.forEach((attr) => {
+    //         const a = attr.data();
+    //         attributes.push(new Attribute(a.text, a.score, attr.id))
+    //     });
+    //     buildAttributes();
+    // });
+
+    db.collection("rewards").get().then((querySnapshot) => {
+        querySnapshot.forEach((reward) => {
+            const r = reward.data();
+            rewards.push(new Reward(r.text, r.id))
+        });
+    });
+}
+
+db.collection("quests").onSnapshot(function(querySnapshot) {
+    quests = [];
+    querySnapshot.forEach((quest) => {
+        const q = quest.data();
+        quests.push(new Quest(q.text, q.duration, q.rewards, q.active, quest.id))
+    });
+    buildQuests();
+});
+
+db.collection("attributes").onSnapshot(function(querySnapshot) {
+    attributes = [];
+    querySnapshot.forEach((attr) => {
+        const a = attr.data();
+        attributes.push(new Attribute(a.text, a.score, attr.id))
+    });
+    buildAttributes();
+});
+
+db.collection("warrior_quests").onSnapshot(function(querySnapshot) {
+    warriorQuests = [];
+    querySnapshot.forEach((quest) => {
+        const q = quest.data();
+        warriorQuests.push(new WarriorQuest(q.warrior, q.quest, q.active, q.completed, q.startDate, quest.id))
+    });
+    buildWarriorQuests();
+});
